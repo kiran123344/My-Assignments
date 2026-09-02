b32771b6caa996db4bcdfe808b55872fbf969700
kiran123344 <95133514+kiran123344@users.noreply.github.com>
2026-09-02 16:53:55 +0530

    Add files via upload

---
+++ javascriptBasics.js
@@ -0,0 +1,15 @@
+let userName = 'kiran'
+console.log(userName)
+console.log(typeof userName)
+let organization = "Organization"
+console.log(organization)
+console.log(typeof organization)
+let contactNumber = 9620077506
+console.log(contactNumber)
+console.log(typeof contactNumber)
+let isJsInteresting = true
+console.log(isJsInteresting)
+console.log(typeof isJsInteresting)
+let usesPlaywright
+console.log(usesPlaywright)
+console.log(typeof usesPlaywright)
